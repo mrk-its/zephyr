6b63488283338e6662e286b872ad5d4380947572
Mariusz Kryński <mrk@sed.pl>
2026-06-10 18:33:12 +0200

    Address reviewer comments:
    - ensure the target reset before we're back in to the caller
    - fix indentation

--- drivers/flash/flash_stm32l5x.c
+++ drivers/flash/flash_stm32l5x.c
@@ -388,7 +388,7 @@ int flash_stm32_option_bytes_write(const struct device *dev, uint32_t mask, uint
 	/* Force the option byte loading (triggers system reset) */
 	regs->NSCR |= FLASH_NSCR_OBL_LAUNCH;
 
-	return 0;
+	return flash_stm32_wait_flash_idle(dev);
 }
 
 uint32_t flash_stm32_option_bytes_read(const struct device *dev)
